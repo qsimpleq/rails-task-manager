bc8f8b5fdc338d33aec018c8b7672220050949f1
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-15 19:37:33 +0500

    rubocop -a

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,20 +3,20 @@ FactoryBot.define do
     first_name
     last_name
     password
-    email { "MyString" }
-    avatar { "MyString" }
-    type { "" }
+    email { 'MyString' }
+    avatar { 'MyString' }
+    type { '' }
 
     factory :admin do
-      type { "Admin" }
+      type { 'Admin' }
     end
 
     factory :developer do
-      type { "Developer" }
+      type { 'Developer' }
     end
 
     factory :manager do
-      type { "Manager" }
+      type { 'Manager' }
     end
   end
 end
