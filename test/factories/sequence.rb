c48c0fc27bd96cb9704e745854575082c1298d22
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-17 12:28:19 +0500

    Update sequence factory

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state] do |n|
     "string#{n}"
   end
 
+  sequence(:expired_at) { (Date.today + 2.months).to_s }
   sequence(:email) { |n| "person_#{n}@example.com" }
+  sequence(:integer, aliases: [:author_id, :assignee_id]) { _1 }
 end
