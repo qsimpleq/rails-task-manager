64e59f014b22c75898ee4a1606435f3d2d536a56
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-17 12:29:13 +0500

    Task and User factory with dynamic values

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
-    name { 'MyString' }
-    description { 'MyText' }
-    author_id { 1 }
-    assignee_id { 1 }
-    state { 'MyString' }
-    expired_at { '2023-05-16' }
+    name
+    description
+    author_id
+    assignee_id
+    state
+    expired_at
   end
 end
