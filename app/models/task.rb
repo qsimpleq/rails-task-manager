1412f95c95989c80967cd79eb7c4fef932a91605
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-05-20 17:07:16 +0500

    Rename events and fix task test

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,27 +8,27 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 500 }
 
   state_machine :state, initial: :new_task do
-    event :develop do
+    event :to_development do
       transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
 
-    event :check_quality do
+    event :to_qa do
       transition in_development: :in_qa
     end
 
-    event :code_review do
+    event :to_code_review do
       transition in_qa: :in_code_review
     end
 
-    event :ready do
+    event :to_ready_for_release do
       transition in_code_review: :ready_for_release
     end
 
-    event :release do
+    event :to_released do
       transition ready_for_release: :released
     end
 
-    event :archive do
+    event :to_archived do
       transition [:new_task, :released] => :archived
     end
   end
