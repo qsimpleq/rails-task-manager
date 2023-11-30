0ae1388ba1c7923dbe08167c150386e9d834ac82
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-11-30 18:19:27 +0500

    Add Admin::UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root :to => "web/boards#show"
 
+  namespace :admin do
+    resources :users
+  end
+
   scope module: :web do
     resource :board, only: :show
     resources :developers, only: [:new, :create]
