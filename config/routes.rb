cdc5dac28bc527ea75e622555f77087825966df4
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-12-01 15:12:16 +0500

    Add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root :to => "web/boards#show"
 
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
   namespace :admin do
     resources :users
   end
